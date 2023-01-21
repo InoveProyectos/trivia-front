b3ca379a877f8c8f68714b0fe575a1a757b1aaeb
SantiRagnar <santbarrios130600@gmail.com>
2023-01-20 22:40:18 -0300

    initial commit y agregando conexion al back con websockets

---
+++ trivia/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css'
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:4000');
+
+function App() {
+
+  return (
+    <div className="App">
+    </div>
+  )
+}
+
+export default App
