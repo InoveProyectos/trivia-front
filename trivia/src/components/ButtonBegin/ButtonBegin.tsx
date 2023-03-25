ac4398b685e810a249238c676e8dc44ccd118fb4
SantiRagnar <santbarrios130600@gmail.com>
2023-03-25 17:54:07 -0300

    Work inicio de la trivia

--- trivia/src/components/ButtonBegin/ButtonBegin.tsx
+++ trivia/src/components/ButtonBegin/ButtonBegin.tsx
@@ -1,7 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import useNotificaiones from "../../hooks/useNotificaiones";
+import useTrivia from "../../hooks/useTrivia";
+import { intRoomData } from "../../interfaces";
 import "./ButtonBegin.scss";
 
-function ButtonBegin() {
-  return <button className="btn btn-begin">Comenzar</button>;
+function ButtonBegin({ roomCode }: intRoomData) {
+  const { startTrivia } = useTrivia();
+  const { errorToast } = useNotificaiones();
+  const navigate = useNavigate();
+
+  const handleStartTrivia = async () => {
+    try {
+      let res: any = await startTrivia(roomCode);
+      console.log({ res });
+      res.length > 0
+        ? navigate("/challenge/:id/:idChallenge")
+        : errorToast("Hubo un error al traer las preguntas");
+    } catch (err) {
+      console.log(err);
+      errorToast("Hubo un error");
+    }
+  };
+
+  return (
+    <button className="btn btn-begin" onClick={handleStartTrivia}>
+      Comenzar
+    </button>
+  );
 }
 
 export default ButtonBegin;
